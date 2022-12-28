813caf7e9c82bdf4b288603754df84b0fe59121c
Ali Ghandali <ali_gh1392@yahoo.com>
2022-12-28 22:05:41 +0330

    add add BuiltinInstaDecoder class and update other files

--- src/farsi_decoder/insta_farsi_decoder.py
+++ src/farsi_decoder/insta_farsi_decoder.py
@@ -7,8 +7,8 @@ from src.data import DATA_DIR
 from src.utils.io import read_json, write_json
 
 
-class InstaChatDecoder:
-    
+class MyInstaChatDecoder:
+
     def __init__(self, chat_json):
         """
         """
@@ -23,8 +23,8 @@ class InstaChatDecoder:
         for k in self.emoji.keys():
             txt = txt.replace(k, self.emoji[k])
         return txt
-    
-    def word_translator(self, txt, unknown_replacement = False):
+
+    def word_translator(self, txt, unknown_replacement=False):
         allowed_letters = string.ascii_letters + '''-><*+!_:..’'"@#''' + "".join(self.emoji.values())
         i = 0
         decoded = ''
@@ -37,18 +37,11 @@ class InstaChatDecoder:
                 i += 1
                 decoded += ' '
                 continue
-                    
+
             if txt[i] in allowed_letters:
                 decoded += txt[i]
                 i += 1
                 continue
-
-            # (Removed) because som characer are 4, 6 or 8 item combined
-            # item = txt[i:i+4]
-            # if self.chat_data.get(item):
-            #     decoded += self.chat_data[item]
-            #     i += 4
-            #     continue
 
             item = txt[i:i+2]
             rep_item = item
@@ -58,9 +51,8 @@ class InstaChatDecoder:
             i += 2
         return decoded
 
-
     def translator(self):
-        result = defaultdict(dict)
+        result = defaultdict(list)
         logger.info("Adding Emojies...")
         logger.info("Farsi Translating...")
         for msg in self.chat_data['messages']:
@@ -70,17 +62,55 @@ class InstaChatDecoder:
                 continue
             if msg['content'] == 'Liked a message':
                 continue
-        
-            result[msg['timestamp_ms']] = {
-                'Time': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(msg['timestamp_ms'] / 1000)),
-                'Sender': self.word_translator(msg['sender_name'], '*'),
-                'Text': self.word_translator(self.emoji_replace(msg['content']), ' ')
-            }
+
+            result["messages"].append(
+                {
+                    "id": msg['timestamp_ms'],
+                    "type": "message",
+                    "date": time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(msg['timestamp_ms'] / 1000)),
+                    "from": self.word_translator(msg['sender_name'], '*'),
+                    "from_id": self.word_translator(msg['sender_name'], '*'),
+                    "text": self.word_translator(self.emoji_replace(msg['content']), ' ')
+                    }
+                    )
         logger.info("Getting Final Result...")
         write_json(result, DATA_DIR / 'result.json')
 
-if __name__ == "__main__":
-    insta_chat = InstaChatDecoder(chat_json=DATA_DIR / 'message_1.json')
-    insta_chat.translator()
 
-    print("*********************************************Done!*********************************************")
+class BuiltinInstaDecoder:
+
+    def __init__(self, chat_json):
+        """
+        """
+        # load chat data
+        logger.info(f"Loading chat data from {chat_json}")
+        self.chat_data = read_json(chat_json)
+
+    def builtin_translator(self):
+        """
+        """
+        result = defaultdict(list)
+        for msg in self.chat_data['messages']:
+            if not msg.get('content'):
+                continue
+            result["messages"].append(
+                {
+                    "id": msg['timestamp_ms'],
+                    "type": "message",
+                    "date": time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(msg['timestamp_ms'] / 1000)),
+                    "from": msg['sender_name'].encode('latin1').decode('utf-8'),
+                    "from_id": msg['sender_name'].encode('latin1').decode('utf-8'),
+                    "text": msg['content'].encode('latin1').decode('utf-8')
+                    }
+                    )
+        logger.info("Getting Final Result...")
+        write_json(result, DATA_DIR / 'result.json')
+
+
+if __name__ == "__main__":
+    # insta_chat = MyInstaChatDecoder(chat_json=DATA_DIR / 'message_1.json')
+    # insta_chat.translator()
+    insta_chat_b = BuiltinInstaDecoder(chat_json=DATA_DIR / 'message_1.json')
+    insta_chat_b.builtin_translator()
+
+    print("*****************************Done!*****************************")
--- src/utils/io.py
+++ src/utils/io.py
@@ -1,12 +1,15 @@
 import json
 
+
 def read_json(file_path):
     with open(file_path) as f:
         return json.load(f)
 
+
 def write_json(data, output_path):
     with open(output_path, 'w') as res:
-            json.dump(data, res, ensure_ascii=False, indent=4)
+        json.dump(data, res, ensure_ascii=False, indent=4)
+
 
 def read_file(file_path):
     with open(file_path) as f:
